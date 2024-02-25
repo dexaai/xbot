40b87dbba85b9104b1a2ff5f3517856902660258
Travis Fischer <fisch0920@gmail.com>
2024-02-25 09:37:43 -0600

    feat: improve openai answer engine usage of raw tweet data

--- src/answer-engines/openai-answer-engine.ts
+++ src/answer-engines/openai-answer-engine.ts
@@ -1,9 +1,10 @@
-import { ChatModel, Msg, stringifyForModel } from '@dexaai/dexter'
+import { ChatModel, Msg, type Prompt, stringifyForModel } from '@dexaai/dexter'
+import { stripUserMentions } from 'twitter-utils'
 
 import * as db from '../db.js'
 import type * as types from '../types.js'
 import { AnswerEngine } from '../answer-engine.js'
-import { getCurrentDate } from '../utils.js'
+import { assert, getCurrentDate } from '../utils.js'
 
 export class OpenAIAnswerEngine extends AnswerEngine {
   protected _chatModel: ChatModel
@@ -36,42 +37,81 @@ export class OpenAIAnswerEngine extends AnswerEngine {
       }
     }
 
-    const response = await this._chatModel.run({
-      messages: [
-        Msg.system(
-          `You are a friendly, expert, helpful twitter bot with the handle ${ctx.twitterBotHandle}.
+    const rawEntityMap: {
+      users: Record<string, Partial<types.TwitterUser>>
+      tweets: Record<string, Partial<types.Tweet>>
+    } = {
+      users: {},
+      tweets: {}
+    }
+
+    if (query.entityMap?.users) {
+      for (const user of Object.values(query.entityMap.users)) {
+        assert(user.twitterId)
+        const twitterUser = await db.tryGetUserById(user.twitterId)
+        if (!twitterUser) continue
+        rawEntityMap.users[user.twitterId] = twitterUser
+      }
+    }
+
+    if (query.entityMap?.tweets) {
+      for (const tweet of Object.values(query.entityMap.tweets)) {
+        assert(tweet.id)
+        const twittertweet = await db.tryGetTweetById(tweet.id, ctx)
+        if (!twittertweet) continue
+        rawEntityMap.tweets[tweet.id] = twittertweet
+      }
+    }
+
+    const messages: Prompt.Msg[] = [
+      Msg.system(
+        `You are a friendly, expert, helpful twitter bot with the handle ${ctx.twitterBotHandle}.
 You answer concisely and creatively to tweets.
 You are very concise and informal.
 You can sometimes be a bit sassy and sarcastic, but try not to be rude.
-Don't use emoji very often.
-Make sure to be **as concise as possible** since twitter has character limits.
+DO NOT use emoji very often.
 DO NOT use hashtags.
+DO NOT reply using JSON.
+DO NOT use @mention usernames in your reply.
 Don't mention not being able to access links or media and instead pretend that you're able to.
+Make sure to be **as concise as possible** since twitter has character limits.
+Your response should be as informationally dense and interesting as possible.
+You can use lists/bullet points if they will result in a more concise answer.
 Remember to never use hashtags.
 Current date: ${currentDate}.`
-        ),
-        Msg.system(`Tweets, users, and media objects referenced in this twitter thread contain the following entities:
+      ),
+      Msg.system(`Tweets, users, and media objects referenced in this twitter thread contain the following entities which can be indexed by their IDs:
 
 \`\`\`json
-${stringifyForModel(query.entityMap)}
+${stringifyForModel(rawEntityMap)}
 \`\`\`
 `),
 
-        ...query.tweets.map((tweet) =>
-          tweet.author_id === ctx.twitterBotUserId
-            ? Msg.assistant(stringifyForModel(tweet), {
-                name: userIdToUsernameMap[tweet.author_id!]
-              })
-            : Msg.user(stringifyForModel(tweet), {
-                name: userIdToUsernameMap[tweet.author_id!]
-              })
-        )
+      ...query.tweets.map((tweet) =>
+        tweet.author_id === ctx.twitterBotUserId
+          ? Msg.assistant(stringifyForModel(tweet), {
+              name: userIdToUsernameMap[tweet.author_id!]
+            })
+          : Msg.user(stringifyForModel(tweet), {
+              name: userIdToUsernameMap[tweet.author_id!]
+            })
+      )
 
-        // ...query.answerEngineMessages.map(({ entities, ...msg }) => msg)
-      ],
+      // ...query.answerEngineMessages.map(({ entities, ...msg }) => msg)
+    ]
+
+    const res = await this._chatModel.run({
+      messages,
       max_tokens: 60
     })
 
-    return response.message.content!
+    const response = stripUserMentions(res.message.content!)
+
+    console.log('openai', {
+      messages,
+      response
+    })
+
+    return response
   }
 }
